b5671b298d06094cac1b17a540447ad64e48335e
unknown <G:\BAT>
2020-05-01 17:40:07 +0300

    Удален мусор

--- js/main.js
+++ js/main.js
@@ -1,7 +1,31 @@
+/* переход по якорям */
 $(document).ready(function(){
-    $('.slider').bxSlider({
-        controls: true,
-        prevText: '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
-        nextText: '<i class="fa fa-chevron-right" aria-hidden="true"></i>'
+    $("a[href*=#]").on("click", function(){
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top
+        }, 1500);
+        return false;
     });
 });
+
+/* всплывающее окно */
+$('.popup .close_window, .overlay_popup').click(function (){
+    $('.popup, .overlay_popup').css({'opacity': 0, 'visibility': 'hidden'});
+});
+$('a.open_window').click(function (e){
+    $('.popup, .overlay_popup').css({'opacity': 1, 'visibility': 'visible'});
+    e.preventDefault();
+});
+
+/* кнопка наверх */
+$(document).ready(function(){
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 200) {
+			$('#back-to-top').fadeIn();
+		} else {
+			$('#back-to-top').fadeOut();
+		}
+	});
+});
+
